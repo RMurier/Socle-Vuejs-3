6f45d77bf5e037444a0f05f84c7c1b01ceb899a3
murierromain@gmail.com <murierromain@gmail.com>
2023-06-08 23:38:29 +0200

    add appbar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
 <v-app>
   <v-main>
+    <AppBar />
     <router-view />
   </v-main>
 </v-app>
 </template>
-
+<style>
+.v-main {
+  display: flex;
+  flex-direction: column;
+}
+</style>
 <script>
+import AppBar from './shared/appbar.vue';
 export default {
   name: 'App',
   watch: {
     $route: {
       immediate: true,
         handler(to, from) {
-            document.title = to.meta.title || 'Romain MURIER';
+            document.title = to.meta.title || 'Socle Vue.JS 3';
         }
       },
   },
-  data: () => ({
-  }),
+  data: () => ({}),
+  components: {
+    AppBar
+  }
 }
 </script>
---
+++ src/shared/appbar.vue
@@ -0,0 +1,31 @@
+<template>
+	<v-app-bar>
+		<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+		<v-toolbar-title>{{title}}</v-toolbar-title>
+	</v-app-bar>
+
+	<v-navigation-drawer v-model="drawer" temporary>
+		<v-list>
+				<div v-for="item in redirectitems">
+					<v-list-item :prepend-icon="item.icon" :title="item.title" @click="redirect(item.path)"></v-list-item>
+				</div>
+		</v-list>
+	</v-navigation-drawer>
+</template>
+<script>
+export default {
+	props: ['title'],
+	name: 'AppBar',
+	data: () => ({
+		drawer: false,
+		redirectitems: [
+			{title: 'Accueil', icon: 'mdi-home', path: "/"}
+		],
+	}),
+	methods: {
+		redirect(path) {
+			this.$router.push(path)
+		}
+	}
+}
+</script>
